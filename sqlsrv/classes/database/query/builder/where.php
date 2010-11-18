5c32978130998f4d6f3c102369487d8361a25564
Sam de Freyssinet <sam@def.reyssi.net>
2010-11-18 15:42:34 -0800

    Fixed the abstract missed

--- sqlsrv/classes/database/query/builder/where.php
+++ sqlsrv/classes/database/query/builder/where.php
@@ -11,7 +11,7 @@
  * @copyright  (c) 2008-2009 Kohana Team
  * @license    http://kohanaphp.com/license
  */
-class Database_Query_Builder_Where extends Kohana_Database_Query_Builder_Where {
+abstract class Database_Query_Builder_Where extends Kohana_Database_Query_Builder_Where {
 
 	/**
 	 * Return up to "LIMIT ..." results
