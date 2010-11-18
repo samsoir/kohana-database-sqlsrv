cfbefb1a449fef5a226ffbf245d6afde53d25029
Sam de Freyssinet <sam@def.reyssi.net>
2010-11-18 15:40:56 -0800

    Deprecated the Database_Query_Builder_Where::limit() method as it is not supported by SQL Server

---
+++ sqlsrv/classes/database/query/builder/where.php
@@ -0,0 +1,28 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Database query builder for WHERE statements.
+ * 
+ * @note       the limit() method has been deprecated as it is
+ *             not supported by SQL Server
+ *
+ * @package    Kohana/Database
+ * @category   Query
+ * @author     Kohana Team
+ * @copyright  (c) 2008-2009 Kohana Team
+ * @license    http://kohanaphp.com/license
+ */
+class Database_Query_Builder_Where extends Kohana_Database_Query_Builder_Where {
+
+	/**
+	 * Return up to "LIMIT ..." results
+	 *
+	 * @param   integer  maximum results to return
+	 * @return  $this
+	 * @deprecated
+	 */
+	public function limit($number)
+	{
+		// This is not supported by MS SQL Server, gracefully ignore this command
+		return $this;
+	}
+}
